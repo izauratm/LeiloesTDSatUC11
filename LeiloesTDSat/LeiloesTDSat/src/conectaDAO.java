8da48d56275dd9272041cf52efcc402395f156a0
izauramoreira <izauratm@gmail.com>
2024-12-05 23:32:21 -0300

    Projeto Inicial UC11

---
+++ LeiloesTDSat/LeiloesTDSat/src/conectaDAO.java
@@ -0,0 +1,25 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adm
+ */
+public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/leiloestds","root","spyke289");
+           
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
