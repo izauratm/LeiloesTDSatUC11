2a494e96be75d34745f18c0295c5508f293beabf
Izauratm <izauratm@gmail.com>
2024-12-14 19:56:35 -0300

    Definida a tela inicial do sistema

--- LeiloesTDSat/LeiloesTDSat/src/TelaPrincipal.java
+++ LeiloesTDSat/LeiloesTDSat/src/TelaPrincipal.java
@@ -3,8 +3,8 @@
  * @author Izaura
  */
 public class TelaPrincipal {
-   public static void main (String[] args){
+    public static void main (String[] args){
        cadastroVIEW tela = new cadastroVIEW();
        tela.setVisible(true);
-   }     
+    }
 }
