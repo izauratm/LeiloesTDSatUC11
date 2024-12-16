66966978ac50aae78d94fcb16862cbb029f8b33c
Izauratm <izauratm@gmail.com>
2024-12-16 04:04:28 -0300

    Correção de bug no método venderProduto() e listarProdutosVendidos renomeado para listarVendidos

---
+++ ProdutosDAO.java
@@ -0,0 +1,111 @@
+/**
+ *
+ * @author Adm
+ */
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class ProdutosDAO { 
+    
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagemVIEW = new ArrayList<>();
+    
+    public ProdutosDAO() {
+        conn = new conectaDAO().connectDB();
+    }
+    public void cadastrarProduto (ProdutosDTO produto){      
+       try {
+            String sql="INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Cadastro foi realizado com sucesso!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
+        } finally {
+            try {
+                if (prep != null) {
+                    prep.close(); 
+                }
+                if (conn != null) {
+                    conn.close(); 
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro ao fechar conexão!" + e.getMessage());
+            }
+        }          
+    }
+    public ArrayList<ProdutosDTO> listarProdutos(){
+    ArrayList<ProdutosDTO> produtos = new ArrayList<>();
+    String sql = "SELECT * FROM produtos"; 
+    try {
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id")); 
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor")); 
+            produto.setStatus(resultset.getString("status"));
+            produtos.add(produto);
+        }
+    } catch (Exception e) {
+        System.err.println("Erro ao listar produtos: " + e.getMessage());
+    } finally {
+        try {
+            if (resultset != null) resultset.close();
+            if (prep != null) prep.close();
+            if (conn != null) conn.close();
+        } catch (Exception e) {
+            System.err.println("Erro ao fechar conexão: " + e.getMessage());
+        }
+    }
+    return produtos;
+    }
+
+    public void venderProduto(int id) {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?useSSL=false, root, spyke289");
+         PreparedStatement prep = conn.prepareStatement(sql)) {
+
+        prep.setInt(1, id);
+        prep.executeUpdate();
+        JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+    } catch (Exception e) {
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + e.getMessage());
+    }
+    }
+    public ArrayList<ProdutosDTO> listarVendidos(){
+        ArrayList<ProdutosDTO> listarVendidos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?useSSL=false", "root", "spyke289");
+             PreparedStatement prep = conn.prepareStatement(sql);
+             ResultSet resultset = prep.executeQuery()) {
+        while (resultset.next()) {
+        ProdutosDTO produto = new ProdutosDTO();
+        produto.setId(resultset.getInt("id"));
+        produto.setNome(resultset.getString("nome"));
+        produto.setValor(resultset.getInt("valor"));
+        produto.setStatus(resultset.getString("status"));
+        listarVendidos.add(produto);
+        }
+           } catch (Exception e) { 
+            e.printStackTrace();    
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + e.getMessage());
+        }
+        return listarVendidos;                     
+        }    
+        }        
---
+++ vendasVIEW.java
@@ -0,0 +1,140 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Izaura
+ */
+public class vendasVIEW extends javax.swing.JFrame {
+
+        public vendasVIEW() {
+        initComponents();
+        listarVendidos();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        listarVendidos = new javax.swing.JTable();
+        jbltitulo = new javax.swing.JLabel();
+        btnVoltar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        listarVendidos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "ID", "Nome", "Valor", "Status"
+            }
+        ));
+        jScrollPane1.setViewportView(listarVendidos);
+
+        jbltitulo.setFont(new java.awt.Font("Lucida Fax", 0, 18)); // NOI18N
+        jbltitulo.setText("Lista de Produtos Vendidos");
+
+        btnVoltar.setText("Voltar");
+        btnVoltar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVoltarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(129, 129, 129)
+                        .addComponent(jbltitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(145, 145, 145)
+                        .addComponent(btnVoltar, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(25, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jbltitulo)
+                .addGap(27, 27, 27)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(btnVoltar)
+                .addContainerGap(59, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
+        cadastroVIEW cadastroVIEW = new cadastroVIEW();
+        cadastroVIEW.setVisible(true);
+       
+    }//GEN-LAST:event_btnVoltarActionPerformed
+    private void listarVendidos() {
+        try { 
+            ProdutosDAO produtosdao = new ProdutosDAO();
+            DefaultTableModel model = (DefaultTableModel)listarVendidos.getModel(); 
+            model.setNumRows(0); 
+            ArrayList<ProdutosDTO> listagem = produtosdao.listarVendidos(); 
+            for (ProdutosDTO produto : listagem) {         
+            model.addRow(new Object[]{ 
+                produto.getId(), 
+                produto.getNome(), 
+                produto.getValor(), 
+                produto.getStatus() 
+            }); 
+            }
+        } catch (Exception e) { 
+            JOptionPane.showMessageDialog(this, "Erro ao listar produtos vendidos: " + e.getMessage()); 
+        }
+    }
+        public static void main(String args[]) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(vendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new vendasVIEW().setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnVoltar;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel jbltitulo;
+    private javax.swing.JTable listarVendidos;
+    // End of variables declaration//GEN-END:variables
+}
+    
